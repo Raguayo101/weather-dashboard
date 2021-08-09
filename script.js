cee2cb6ae300303fe2a4da95a0649fc066bf7773
Drigo <rodrigoaguayo94@gmail.com>
2021-08-09 09:47:05 -0700

    added html elements to our API info

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
+// trying to get our forcast
+function getWeather(userInput) {
+  if (!userInput) {
+    return;
+  }
+  var apiEndPoint = `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=cd979f38b9769fc27e8605c58c631c3d&units=imperial`;
+  fetch(apiEndPoint)
+    .then((res) => res.json())
+    .then((data) => {
+      // select our forsecast and add a header to it
+      var weatherEl = document.querySelector('#weather');
+      weatherEl.innerHTML = '<h4 class ="mt-3">5-day Forecast:</h4>';
 
-// trying to get our forcast 
-function getWeather(userInput){
-    if(!userInput){
-        return;
-    }
-    var apiEndPoint = `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=cd979f38b9769fc27e8605c58c631c3d&units=imperial`;
-    fetch(apiEndPoint)
-        .then((res) => res.json())
-        .then((data) => {
-            // select our forsecast and add a header to it
-            var weatherEl = document.querySelector('#weather');
-            weatherEl.innerHTML = '<h4 class ="mt-3">5-day Forecast:</h4>';
+      weatherRowEl = document.createElement('div');
+      weatherRowEl.className = '"row"';
 
-            weatherRowEl = document.createElement('div');
-            weatherRowEl.className = '"row"';
+      // going to start looping through the forecasts
+      for (var i = 0; i < data.list.length; i++) {
+        if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
+          //creating our html elements
+          var columnEl = docuement.createElement('div');
+          columnEl.classList.add('col-md-2');
 
-            // going to start looping through the forecasts
-            for (var i = 0; i <data.list.length; i++){
-                if(data.list[i].dt_txt.indexOf('15:00:00')!== -1){
-                    //creating our html elements
-                    
-                }
-            }
-        });
+          var cardEl = docuement.createElement('div');
+          cardEl.classList.add('card', 'bg-primary', 'text-white');
+
+          var windsEl = docuement.createElement('p');
+          windEl.classList.add('card-text');
+          windsEl.textContent = `Wind Speed: ${data.list[i].wind.speed} MPH`;
+
+          var humidityEl = docuement.createElement('p');
+          humidityEl.classList.add('card-text');
+          humidityEl.textContent = `humidity: ${data.list[i].main.humidity} MPH`;
+
+          var bodyEl = document.createElement('div');
+          bodyEl.classList.add('card-body', 'p-2');
+
+          var titleEl = document.createElement('h5');
+          titleEl.classList.add('card-title');
+          titleEl.textContent = new Date(
+            data.list[i].dt_txt
+          ).toLocaleDateString();
+
+          var imgEl = document.createElement('img');
+          imgEl.setAttribute(
+            'src',
+            `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
+          );
+
+          var p1El = document.createElement('p');
+          p1El.classList.add('card-text');
+          p1El.textContent = `Temp: ${data.list[i].main.temp_max} °F`;
+
+          var p2El = document.createElement('p');
+          p2El.classList.add('card-text');
+          p2El.textContent = `Humidity: ${data.list[i].main.humidity}%`;
+        }
+      }
+    });
 }
-
-
